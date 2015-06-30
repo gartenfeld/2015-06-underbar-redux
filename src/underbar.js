6f75151cbf6460c49afde3802748cc6ee7283033
gartenfeld <david@rosson.com.au>
2015-06-30 10:18:23 -0700

    Implemented Part I

--- src/underbar.js
+++ src/underbar.js
@@ -101,6 +101,14 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var returnArr = [];
+    _.each(array, function(item) {
+      if (_.indexOf(returnArr, item)===-1) {
+        returnArr.push(item);
+      }
+    });
+    return returnArr;
+    //GOOD LUCK!
   };
 
 
@@ -109,6 +117,12 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var returnArr = [];
+    _.each(collection, function(item){
+      returnArr.push(iterator(item));
+    });
+
+    return returnArr;
   };
 
   /*
@@ -150,6 +164,13 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator === undefined) {
+      accumulator = collection.splice(0,1)[0];
+    }
+    _.each(collection, function(item){
+      accumulator = iterator(accumulator,item);
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
