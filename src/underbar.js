5442ae4d270da64990d0d5c94c3409d74bccfa41
gartenfeld <david@rosson.com.au>
2015-06-30 16:30:20 -0700

    Added invoke

--- src/underbar.js
+++ src/underbar.js
@@ -358,6 +358,12 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item){
+      if (typeof functionOrKey === 'string'){
+        functionOrKey = String.prototype[functionOrKey];
+      }
+      return functionOrKey.apply(item);
+    });
   };
 
   // Sort the object's values by a criterion produced by an iterator.
